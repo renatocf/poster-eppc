92e155a703bb748e09533f1618cc188cd4e791bb
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-11-06 16:54:28 -0200

    Fix style issues with ESLint + Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off",
+    "no-debugger": "off",
+    "vue/max-attributes-per-line": "off"
   },
+
   parserOptions: {
     parser: "babel-eslint"
-  }
+  },
+
+  extends: ["plugin:vue/recommended", "@vue/prettier"]
 };
